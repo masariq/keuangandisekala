cd8025147858dd9679c3193b15533a9bc624fb2e
masariq <rafiariqmaulana48@gmail.com>
2026-08-15 10:07:40 +0700

    Fix function search path

---
+++ supabase/migrations/20260815030720_0005_fix_function_search_path.sql
@@ -0,0 +1,11 @@
+-- Fix mutable search_path on update_updated_at function
+CREATE OR REPLACE FUNCTION public.update_updated_at()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SET search_path = ''
+AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$;
